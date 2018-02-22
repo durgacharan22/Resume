2b186b01567e935b44b824dd4171a292c01010b0
Potukuru,Durga Charan <s530748@ch112024m.nwmissouri.edu>
2018-02-22 15:45:36 -0600

    Initial Commit

---
+++ Resume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Resume
+//
+//  Created by Potukuru,Durga Charan on 2/22/18.
+//  Copyright © 2018 Potukuru,Durga Charan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
